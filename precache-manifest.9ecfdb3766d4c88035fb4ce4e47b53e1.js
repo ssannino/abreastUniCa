7b608360251afcf82c567a73dfd5e3dd8b92b406
ssannino <ssannino@users.noreply.github.com>
2022-06-10 14:23:01 +0000

    published by GitHub Actions

---
+++ precache-manifest.9ecfdb3766d4c88035fb4ce4e47b53e1.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd1c74d804852a27bbb01f639c4236fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85aafdda17e85472275e",
+    "url": "/static/css/2.94ed5f3f.chunk.css"
+  },
+  {
+    "revision": "bb6eb3a7698b9ba55a64",
+    "url": "/static/css/main.fd1a0bee.chunk.css"
+  },
+  {
+    "revision": "85aafdda17e85472275e",
+    "url": "/static/js/2.3b8e2c8e.chunk.js"
+  },
+  {
+    "revision": "dc6e20814c238fe27d42db1b819e8882",
+    "url": "/static/js/2.3b8e2c8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb6eb3a7698b9ba55a64",
+    "url": "/static/js/main.da081e19.chunk.js"
+  },
+  {
+    "revision": "aeae52f5ee6382d9116d",
+    "url": "/static/js/runtime-main.70e6daa0.js"
+  },
+  {
+    "revision": "f36168da5d6b38f8723fa1f2ccaf288f",
+    "url": "/static/media/Cabin-Bold.f36168da.ttf"
+  },
+  {
+    "revision": "663f55b214418d4c55b85c6fc32ab18a",
+    "url": "/static/media/Cabin-Medium.663f55b2.ttf"
+  },
+  {
+    "revision": "548e005a375c047eb8bd9485508eb58a",
+    "url": "/static/media/Cabin-Regular.548e005a.ttf"
+  },
+  {
+    "revision": "2f637f2b06f15cbd8da7d284d6def095",
+    "url": "/static/media/aperitivo80ml.2f637f2b.png"
+  },
+  {
+    "revision": "5e2b10ff1b1e9c14e98cd5bdab88540b",
+    "url": "/static/media/bra.5e2b10ff.svg"
+  },
+  {
+    "revision": "318625894d156d9149ad66a9efb96c4d",
+    "url": "/static/media/check-icon.31862589.svg"
+  },
+  {
+    "revision": "2c73ffc1db7c9b468e0e944c8b4d4230",
+    "url": "/static/media/cross-hover.2c73ffc1.svg"
+  },
+  {
+    "revision": "4351d2389a55c979c299c77721ab613d",
+    "url": "/static/media/cross.4351d238.svg"
+  },
+  {
+    "revision": "f24145ee850b184219865f55592d593c",
+    "url": "/static/media/deodorant.f24145ee.svg"
+  },
+  {
+    "revision": "838c9965ddb9bfa1e081e7b70430b233",
+    "url": "/static/media/drinking.838c9965.svg"
+  },
+  {
+    "revision": "cc3263c216dba3064cfb65edf24396c1",
+    "url": "/static/media/drinks.cc3263c2.svg"
+  },
+  {
+    "revision": "f74f3f9f9365a40e3717b7057fb5caf3",
+    "url": "/static/media/drinks.f74f3f9f.svg"
+  },
+  {
+    "revision": "007d2df4efc265ba6ebf5cf31f06f940",
+    "url": "/static/media/dropdown-arrow-simple.007d2df4.svg"
+  },
+  {
+    "revision": "b85f1eba327e718ebaf684bce19d0d76",
+    "url": "/static/media/dropdown-arrow.b85f1eba.svg"
+  },
+  {
+    "revision": "2788e74932ae8176230f21cedaaf5878",
+    "url": "/static/media/error-icon.2788e749.svg"
+  },
+  {
+    "revision": "7f54ede563eeac5c0c07dd42d2a19bc6",
+    "url": "/static/media/half-wine.7f54ede5.svg"
+  },
+  {
+    "revision": "679dc7e18ae577bd7e6052c84784c0d2",
+    "url": "/static/media/height.679dc7e1.svg"
+  },
+  {
+    "revision": "499a05709c5d57f35b4d71dfa78b03b0",
+    "url": "/static/media/hrt.499a0570.svg"
+  },
+  {
+    "revision": "06b6bc317d0fb504ccef0e24d31c08d1",
+    "url": "/static/media/keep-breasts-healthy.06b6bc31.svg"
+  },
+  {
+    "revision": "d7c45db71003febd270685361b6de770",
+    "url": "/static/media/logo@3x.d7c45db7.png"
+  },
+  {
+    "revision": "4f7ac0030554ce5dbe316d42e87d761b",
+    "url": "/static/media/minus.4f7ac003.svg"
+  },
+  {
+    "revision": "65d99181ed76cd9d50f1fbb3aa8d7aeb",
+    "url": "/static/media/myths.65d99181.svg"
+  },
+  {
+    "revision": "82b1a784eaf78759dd88b109ac21b6b2",
+    "url": "/static/media/nurse.82b1a784.svg"
+  },
+  {
+    "revision": "78a7be6b90c68e149c53eaef09253dd6",
+    "url": "/static/media/physical-activity.78a7be6b.svg"
+  },
+  {
+    "revision": "fe655ceba724b6b50b32715594c46cc0",
+    "url": "/static/media/physical-activity.fe655ceb.svg"
+  },
+  {
+    "revision": "b493372b5588d0ecf4b0d55a3abd1a86",
+    "url": "/static/media/pint-a.b493372b.svg"
+  },
+  {
+    "revision": "c8bc09b18ff58c8781a2af29dda15699",
+    "url": "/static/media/plus.c8bc09b1.svg"
+  },
+  {
+    "revision": "ce962b3e84713bacd05724400418e59a",
+    "url": "/static/media/question.ce962b3e.svg"
+  },
+  {
+    "revision": "8ea4294f4898411c7f5f182dbcff3c56",
+    "url": "/static/media/risks.8ea4294f.svg"
+  },
+  {
+    "revision": "8a507567eb1edc1b15db25a2dfb9004d",
+    "url": "/static/media/scales.8a507567.svg"
+  },
+  {
+    "revision": "171b90a0d9389f5ea5ac65cb5f916d54",
+    "url": "/static/media/single-spirit.171b90a0.svg"
+  },
+  {
+    "revision": "fd92df7a8efde584987c25fc9d7c023e",
+    "url": "/static/media/smoker.fd92df7a.svg"
+  },
+  {
+    "revision": "66dce4a393e227b2475c94f6cef0a866",
+    "url": "/static/media/smoking.66dce4a3.svg"
+  },
+  {
+    "revision": "5c041d37b26013ace427c066ef1aa95e",
+    "url": "/static/media/spirit-double.5c041d37.svg"
+  },
+  {
+    "revision": "8feed3d758dd337af2e56f36cb9fd132",
+    "url": "/static/media/text-warning-icon.8feed3d7.svg"
+  },
+  {
+    "revision": "297d6d4654db618606f4ec8fd68c46c0",
+    "url": "/static/media/tick.297d6d46.svg"
+  },
+  {
+    "revision": "7eb08525091f82e7770c2b7c47aaca6c",
+    "url": "/static/media/time.7eb08525.svg"
+  },
+  {
+    "revision": "ae2923d65d47a697ef8c018f9778878e",
+    "url": "/static/media/tips.ae2923d6.svg"
+  },
+  {
+    "revision": "7b4af2a0277627c696dc75a38d691dd0",
+    "url": "/static/media/unit-triangle.7b4af2a0.png"
+  },
+  {
+    "revision": "46ecb427d069db9e698fd11b85a381b9",
+    "url": "/static/media/unit-triangle@2x.46ecb427.png"
+  },
+  {
+    "revision": "99b00fbc7ff31157cd244da020f2066e",
+    "url": "/static/media/unit-triangle@3x.99b00fbc.png"
+  },
+  {
+    "revision": "7d4f0479c800a98026e2dbccb4c3ca39",
+    "url": "/static/media/unitGraph.7d4f0479.png"
+  },
+  {
+    "revision": "ee91bc75fb522e3b8c0dce665cac5a69",
+    "url": "/static/media/unitGraph@2x.ee91bc75.png"
+  },
+  {
+    "revision": "41118cd564c9db56ebc7cd420dfed830",
+    "url": "/static/media/unitGraph@3x.41118cd5.png"
+  },
+  {
+    "revision": "367fa412a0c314b6f55fc554aed9b19a",
+    "url": "/static/media/watermelon.367fa412.svg"
+  },
+  {
+    "revision": "5316a4d83d87510f31916cc019f2b892",
+    "url": "/static/media/weight.5316a4d8.svg"
+  },
+  {
+    "revision": "58b210145058d54e44230f3aee524ab5",
+    "url": "/static/media/weight.58b21014.svg"
+  },
+  {
+    "revision": "65c7aba829c10d72a7d5b85666bef224",
+    "url": "/static/media/wellbeing.65c7aba8.svg"
+  },
+  {
+    "revision": "ac0b7dc047840878847f903fb3d161e8",
+    "url": "/static/media/wellbeing.ac0b7dc0.svg"
+  },
+  {
+    "revision": "158e3ad2004e070d6a657e3407bbb9c8",
+    "url": "/static/media/white_remove.158e3ad2.svg"
+  },
+  {
+    "revision": "a6760d5b5226998dec0c506d071b8b1c",
+    "url": "/static/media/wine-175ml.a6760d5b.svg"
+  }
+]);
